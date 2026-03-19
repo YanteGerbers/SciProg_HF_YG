7645c272fdc6aed3c11bd398e08e93aacc875d54
Yante Gerbers <yante.2001@gmail.com>
2026-03-19 17:57:30 +0100

    mmodified Initialize_system subroutine

--- src/Initialize_system.F90
+++ src/Initialize_system.F90
@@ -9,6 +9,7 @@ contains
     subroutine define_molecule(molecule)
      ! This routine should be improved such that an arbitrary molecule can be given as input
      ! the coordinates below are for a be-he dimer oriented along the x-axis with a bond length of 2 au
+     !!!!! The coordinates of the shell centers are the nuclear coordinates
      type(molecular_structure_t), intent(inout) :: molecule
      real(8) :: charge(2),coord(3,2)
      charge(1)   = 4.D0
@@ -18,18 +19,46 @@ contains
      call add_atoms_to_molecule(molecule,charge,coord)
    end subroutine
 
-   subroutine define_basis(ao_basis)
-    ! This routine can be extended to use better basis sets 
-    ! The coordinates of the shell centers are the nuclear coordinates
-    ! Think of a refactoring of define_molecule and define_basis to ensure consistency 
+   subroutine define_basis(ao_basis, molecule)
+    ! This routine was extended to use more extensive basis sets, according to assigment,
+    ! and modified to be consistent with define_molecule.
+    ! The coordinates of the shell centers are the nuclear coordinates (of the atoms)
      type(basis_set_info_t), intent(inout) :: ao_basis
-     type(basis_func_info_t) :: gto
-     ! Be:  2 uncontracted s-funs:    l      coord          exp      
-     call add_shell_to_basis(ao_basis,0,(/0.D0,0.D0,0.D0/),4.D0)
-     call add_shell_to_basis(ao_basis,0,(/0.D0,0.D0,0.D0/),1.D0)
-     ! He:  1 uncontracted s-fun:     l      coord          exp      
-     call add_shell_to_basis(ao_basis,0,(/2.D0,0.D0,0.D0/),1.D0)
+     type(molecular_structure_t), intent(in) :: molecule
+     !!!!! type(basis_func_info_t) :: gto
+     integer :: i
+     do i = 1, molecule%num_atoms
+       if (molecule%charge(i) == 1) then
+         ! H: 3 uncontracted s-shells:    l          coord      exp     3x1=3funcs  (1func/shell)
+         call add_shell_to_basis(ao_basis,0,molecule%coord(:,i),0.1D0)
+         call add_shell_to_basis(ao_basis,0,molecule%coord(:,i),1.0D0)
+         call add_shell_to_basis(ao_basis,0,molecule%coord(:,i),3.0D0)
+       else
+         ! non-H:
+         !    5 uncontracted s-shells:    l          coord      exp      5x1=5funcs (1func/shell)
+         call add_shell_to_basis(ao_basis,0,molecule%coord(:,i),0.10D0)
+         call add_shell_to_basis(ao_basis,0,molecule%coord(:,i),0.35D0)
+         call add_shell_to_basis(ao_basis,0,molecule%coord(:,i),1.0D0)
+         call add_shell_to_basis(ao_basis,0,molecule%coord(:,i),3.0D0)
+         call add_shell_to_basis(ao_basis,0,molecule%coord(:,i),10.0D0)
+         !    3 uncontracted p-shells:    l          coord      exp      3x3=9funcs (3func/shell, px,py,pz)
+         call add_shell_to_basis(ao_basis,1,molecule%coord(:,i),0.2D0)
+         call add_shell_to_basis(ao_basis,1,molecule%coord(:,i),1.0D0)
+         call add_shell_to_basis(ao_basis,1,molecule%coord(:,i),5.0D0)
+         !    1 uncontracted d-shell:     l          coord      exp      1x6=6funcs (6funcs/shell, xx, yy, zz, xy, xz, yz) 
+         call add_shell_to_basis(ao_basis,2,molecule%coord(:,i),1.0D0)
+       end if
+     end do
    end subroutine
+
+
+
+
+
+
+
+
+
 
    subroutine initialize_system(molecule, ao_basis, Ne, Nocc, NAO)
     ! This routine .................. 
